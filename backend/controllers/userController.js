0f3c4bc62a70a6a9daf9fa117dfe10c285940aca
Jason Pennini <jason.pennini@gmail.com>
2024-09-07 13:52:34 -0700

    created userModel and userController with bcrypt authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+const { json } = require('express')
 const User = require('../models/userModel')
 
 // login user
@@ -6,9 +7,21 @@ const loginUser = async (req, res) => {
 }
 
 // signup user
-
 const signupUser = async (req, res) => {
-  return res.json({mssg:"signup user"})
+  //destructuring off req body
+  const {email, password} = req.body
+  try {
+    // using User model's signup method and inserting the email and password from the request body as arguments into it
+    // this will create an object user with the email and password form the request on the front end
+    // taking the hashed password
+
+    const user = await User.signup(email, password)
+    return res.status(200).json({email, user})
+
+  } catch (error) {
+    // we could have a mongoose error here
+    return res.status(400).json({error:error.message})
+  }
 }
 
 module.exports = {loginUser, signupUser}
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +14,30 @@ const userSchema = new Schema({
     required: true,
   }
 })
+
+// static signup method
+
+// cannot be arrow function b/c we are using the 'this' keyword
+userSchema.statics.signup = async function (email, password) {
+  // checking the DB for the email 
+  const exists = await this.findOne({email})
+  if(exists) {
+    throw Error("The email already exists in DB")
+  }
+  // Brcypt is a hasing function that makes password in DB harder to crack
+  // Brcypt users salt which randomizes a string to the user's password before it is hashed
+  // mypassword930csdf32n09d
+  // mypassword39x03mc920nd9
+
+  // the number in genSalt will be home many times randomization occurs
+  const salt = await bcrypt.genSalt(10)
+  // two args are plain text password, and salt value
+  const hash = await bcrypt.hash(password, salt)
+
+  // creating a new document in this, which is the DB model, using the email and hashed password.
+  const user = await this.create({email, password:hash})
+
+  return user
+}
 
 module.exports = mongoose.model('User', userSchema)
