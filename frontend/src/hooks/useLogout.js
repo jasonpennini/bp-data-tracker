26ee5b33fcb8c39e7a642aceab9fcb536e19064b
Jason Pennini <jason.pennini@gmail.com>
2024-09-12 16:21:22 -0700

    protect routes based on user login and only display BP Entries for users who created them

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,13 +1,16 @@
 import { useAuthContext } from './useAuthContext'
+import { useBPContext } from './useBPEntriesContext'
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext()
+  const { dispatch:bpDispatch } = useBPContext()
 
   const logout = () => {
     
     // we need to update global state and remove JWT to log out
     localStorage.removeItem('user')
     dispatch({type:'LOGOUT'})
+    bpDispatch({type: 'SET_BPENTRIES', payload:null})
   }
   return {logout}
 }
