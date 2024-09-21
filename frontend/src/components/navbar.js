ca84e2fd3862bdda594fbf57489f1a64406a25e9
Jason Pennini <jason.pennini@gmail.com>
2024-09-21 14:26:54 -0700

    WIP updated navbar with bootstrap

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -13,25 +13,54 @@ const Navbar = () => {
   return (
     <header>
       <div className="container">
-        <Link to='/'>
+        <Link to="/">
           <h1>Batting Practice Tracker</h1>
         </Link>
-        <nav> 
-          {/* if user is truthy/ie user exists, then output the user email */}
-          {user && (
-            <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                 <Link to='/bpEntry'>BP Entry</Link>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Custom Reports
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Max EV Leaderboards
+                  </a>
+                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a className="dropdown-item" href="#">Coach Pitch</a></li>
+                    <li><a className="dropdown-item" href="#">Blackbox</a></li>
+                    <li><a className="dropdown-item" href="#">High Velocity</a></li>
+                    <li><a className="dropdown-item" href="#">Situational</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex">
+                <input className="form-control me-2" type="Player Search" placeholder="Player Search" aria-label="Player Search" />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+              {user && (
+              <div>
               <span>{user.email}</span>
               <button onClick={handleClick}> Logout </button>
+              </div>
+              )}
+              {/* if user is truthy/ie user exists, then output the user email */}
+              {!user && (
+              <div>
+                <Link to='/login'>Login</Link>
+                <Link to='/signup'>Signup</Link>
+              </div>
+              )}
             </div>
-          )}
-          {/* if user is truthy/ie user exists, then output the user email */}
-          {!user && (
-            <div>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Signup</Link>
           </div>
-          )}
-          
         </nav>
       </div>
     </header>
